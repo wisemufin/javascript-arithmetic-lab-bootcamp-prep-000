5710457dfc93ae87b3a2872b21453ba7bb637f4b
Matthew Trabold <mct4240@gmail.com>
2019-01-14 19:28:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,3 +4,15 @@ var b = 15
 function add(a, b) {
   return(a + b)
 }
+
+function subtract(a, b) {
+  return(a - b)
+}
+
+function multiply(a, b) {
+  return(a * b)
+}
+
+function divide(a, b) {
+  return
+}
