b9cbea72a8538925871f3c9c97d6ba72a07b0ab9
Matthew Trabold <mct4240@gmail.com>
2019-01-14 19:37:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,11 +17,13 @@ function divide(a, b) {
   return(a / b)
 }
 
-function inc(a) {
-  return a++
+var n = 2
+
+function inc(n) {
+  return n += 1
 }
 
-function dec(a) {
-  return a--
+function dec(n) {
+  return n -= 1
 }
 
